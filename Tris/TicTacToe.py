dedfd30f210efd8668321e6c34d95574e4d954cd
giovannetor <giovanni.coci1@yahoo.com>
2021-07-27 00:06:32 +0200

    version 2.0. Programma finito e funzionante, aggiunte stringhe in inglese. mancano regole e aiuti.

--- Tris/TicTacToe.py
+++ Tris/TicTacToe.py
@@ -1,40 +1,59 @@
-import os
-import random
 import threading
 import time
-from datetime import datetime
-from enum import Enum
-from io import StringIO
-from pathlib import Path
-
 import sopel.plugin as plugin
 import sopel.tools as tools
 from sopel.formatting import colors, CONTROL_BOLD, CONTROL_COLOR, CONTROL_NORMAL
 
-X = CONTROL_BOLD + CONTROL_COLOR + colors.LIGHT_CYAN + CONTROL_NORMAL
-O = CONTROL_BOLD + CONTROL_COLOR + colors.RED + CONTROL_NORMAL
+X = CONTROL_BOLD + CONTROL_COLOR + colors.LIGHT_CYAN + "X" + CONTROL_NORMAL
+O = CONTROL_BOLD + CONTROL_COLOR + colors.RED + "O" + CONTROL_NORMAL
+TRIS = CONTROL_BOLD + CONTROL_COLOR + colors.RED + "," + colors.WHITE + " Tic" + CONTROL_COLOR + colors.BLACK \
+       + "," + colors.WHITE + "Tac" + CONTROL_COLOR + colors.LIGHT_CYAN + "," + colors.WHITE + "Toe " + CONTROL_NORMAL
+
 gridlist = ["A1" , "A2"  , "A3" , "B1"   ,"B2"  , "B3" , "C1" , "C2", "C3"]
 
 log_chan = "#trinacry-logs"
-game_chan = ["#TicTacToe"]
+game_chan = ["#TicTacToe" , "#games"]
 lock = threading.RLock()
 max_player = 2
 
+strings_eng = {"impos_unirsi": "I'm sorry %s , the max number of players is 4. Wait until next match :)",
+               "cant_play": "You are not inside the match, please do not disturb the other players :)",
+               "start" : "Players Reached! Let's play " + TRIS + "!!",
+               "gia_dentro": " %s you are already inside the match of " + TRIS + ".",
+               "turno" : "%s 's turn, please wait.",
+               "noletter" : "You typed '%s', must type A, B or C instead.",
+               "nonumber" : "You typed '%s', musts be 1, 2 or 3 instead.",
+               "nogrid" : "'%s' isn't a proper slot in the grid.",
+               "yours" : "This slot is already yours!",
+               "altro" : "This slot belongs to the other player.",
+               "ok" : "%s in %s by %s!",
+               "win" : "%s made " + TRIS + "!!",
+               "next_turn" : "It's %s 's turn now!",
+               "final_win" : "%s scored 3 wins and won the " + TRIS + " game!!",
+               "endgame" : CONTROL_BOLD + CONTROL_COLOR + colors.RED + "GAME OVER"
+                           + CONTROL_NORMAL + ". Start a new match if you wanna play again.",
+               "game_started" : "A new game of " + TRIS + " is started!",
+               "adstop" : CONTROL_BOLD + CONTROL_COLOR + colors.RED + "GAME OVER"
+                           + CONTROL_NORMAL + ". The game was stopped by an Admin.",
+               "not_started" : "The match hasn't started yet. Find another player to start.",
+               "no_square" : "Please put a slot after the command. e.g. 'A2'"}
+
 class tttgame:
     def __init__(self , trigger):
-        self.strings = strings_eng
-        self.string_help = string_help_eng
-        self.rules = rules_eng
+        self.strings = strings_eng  # default strings are in english
+        #self.string_help = string_help_eng
+        #self.rules = rules_eng
         self.starter = trigger.nick
         self.channel = trigger.sender
-        self.players = {self.starter: {"squares" : [] , "sign" : X}}  # player's dict. each player will have a card list
-        self.playerOrder = [self.starter]  # player order. it will mostly be used paired with self.currentPlayer
+        self.players = {}  # Player's dict
+        self.playerOrder = []  # List of players per order
         self.currentPlayer = 0
         self.startTime = None
         self.dealt = False
-        self.dicgrid =  {"A1" : "/" , "A2" : "/" , "A3" : "/",
+        self.griglia =  {"A1" : "/" , "A2" : "/" , "A3" : "/",   # game grid
                          "B1" : "/"  ,"B2" : "/" , "B3" : "/" ,
                          "C1" : "/" , "C2" : "/" , "C3" : "/"}
+        self.jcount = 0
 
     def join(self, bot, trigger):
         with lock:
@@ -45,30 +64,46 @@ class tttgame:
                 if self.dealt:
                     bot.say(self.strings["cant_play"])
                     return
-                self.players[trigger.nick]= {"squares" : [] , "sign" : O}  # add player to players dict
+                if self.jcount == 0:
+                    self.players[trigger.nick]= {"squares" : [] , "sign" : X , "score" : 0}
+                else:
+                    self.players[trigger.nick]= {"squares" : [] , "sign" : O , "score" : 0}  # add player to players dict
+                self.jcount += 1
                 self.playerOrder.append(trigger.nick)  # add player to players order list
-                bot.say(self.strings['inizio'])  # at least 2 players, ready to deal
-                self.dealt = True
+                if len(self.players) == 2:
+                    self.dealt = True
+                    bot.say(self.strings["start"])
+                    self.print_grid(bot)
+                    bot.say(self.strings['turno'] % self.playerOrder[self.currentPlayer])
             else:
                 bot.say(self.strings["gia_dentro"] % trigger.nick)  # player already in
 
     def play(self, bot, trigger):
         player = trigger.nick
+        if not self.dealt:
+            bot.say(self.strings["not_started"])
+            return
         if player not in self.players:
             bot.notice(self.strings['cant_play'], player)
             return
         if player != self.playerOrder[self.currentPlayer]:
             bot.say(self.strings['turno'] % self.playerOrder[self.currentPlayer])
             return
+        if not trigger.group(3):
+            bot.say(self.strings["no_square"])
+            return
 
-        cmd = trigger.group(3).upper
+        cmd = trigger.group(3).upper()
+        print(cmd)
 
         if cmd[0] not in "ABC":
-            bot.say(self.strings["noletter"] % cmd)
+            bot.say(self.strings["noletter"] % cmd[0])
             return
 
         try: int(cmd[1])
-        except:  bot.say(self.strings["nonumber"] % cmd[1])
+        except:
+            bot.say(self.strings["nonumber"] % cmd[1])
+            return
 
         if cmd not in gridlist:
             bot.say(self.strings["nogrid"] % cmd)
@@ -76,26 +111,57 @@ class tttgame:
 
         cp = self.check_possible(player , cmd)
         if cp == 0:
-            bot.say(self.strings["già_tuo"])
+            bot.say(self.strings["yours"])
             return
         elif cp == 2:
             bot.say(self.strings["altro"])
             return
 
-        bot.say(self.strings["ok"])
+        bot.say(self.strings["ok"] % (self.players[player]["sign"] , cmd , player))
 
         self.players[player]["squares"].append(cmd)
-
+        self.griglia[cmd] = self.players[player]["sign"]
         self.print_grid(bot)
 
         if self.checkwin(player):
             bot.say(self.strings["win"] % player)
+            self.players[player]["score"] += 1
+            self.reboot(bot , place = trigger.sender)
         else:
             self.currentPlayer += 1
-            if self.currentPlayer > 1:
+            if self.currentPlayer == len(self.playerOrder):
                self.currentPlayer = 0
-            bot.say(self.strings["next_turn"])
+            bot.say(self.strings["next_turn"] % self.playerOrder[self.currentPlayer])
 
+    def reboot(self , bot , place):
+        for player in self.players:
+            if self.players[player]["score"] == 3:
+                bot.say(self.strings["final_win"] % (player))
+                self.endgame(bot , player_win = player , place = place)
+                return
+            self.players[player]["squares"].clear()
+
+            if self.players[player]["sign"] == X:
+                self.players[player]["sign"] = O
+            else:
+                self.players[player]["sign"] = X
+
+            self.currentPlayer += 1
+            if self.currentPlayer == len(self.playerOrder):
+               self.currentPlayer = 0
+
+            self.griglia = {"A1" : "/" , "A2" : "/" , "A3" : "/",
+                         "B1" : "/"  ,"B2" : "/" , "B3" : "/" ,
+                         "C1" : "/" , "C2" : "/" , "C3" : "/"}
+
+            self.print_grid(bot)
+            bot.say(self.strings['turno'] % self.playerOrder[self.currentPlayer])
+
+
+    def endgame(self , bot, player_win , place):
+        bot.say(self.strings["endgame"])
+        forced = False
+        tttbot.endgame( bot ,player_win,  place , forced )
 
     def checkwin(self , player):
         squ =  self.players[player]["squares"]
@@ -115,22 +181,90 @@ class tttgame:
             return False
 
     def check_possible(self , player , cmd):
-        if gridlist[cmd] == self.players[player]["sign"]:
+        if self.griglia[cmd] == self.players[player]["sign"]:
             return 0
         else:
-            if gridlist[cmd] == "/":
+            if self.griglia[cmd] == "/":
                 return 1
             else:
                 return 2
 
     def print_grid(self , bot):
-        dicgrid = self.dicgrid
-        bot.say(dicgrid["A1"]+ "   |   " + dicgrid["A2"] + "   |   " + dicgrid["A3"])
-        bot.say("------------------------------")
-        bot.say(dicgrid["B1"]+ "   |   " + dicgrid["B2"] + "   |   " + dicgrid["B3"])
-        bot.say("------------------------------")
-        bot.say(dicgrid["C1"]+ "   |   " + dicgrid["C2"] + "   |   " + dicgrid["C3"])
+        dicgrid = self.griglia
+        bot.say(dicgrid["A1"] + "   |   " + dicgrid["A2"] + "   |   " + dicgrid["A3"])
+        bot.say("-------------")
+        bot.say(dicgrid["B1"] + "   |   " + dicgrid["B2"] + "   |   " + dicgrid["B3"])
+        bot.say("-------------")
+        bot.say(dicgrid["C1"] + "   |   " + dicgrid["C2"] + "   |   " + dicgrid["C3"])
+
+
+class tttbot:
+    def __init__(self):
+        self.games = {}
+        self.strings = strings_eng
+
+    def start(self, bot, trigger):
+
+        if trigger.sender in self.games:
+            self.join(bot, trigger)
+        else:
+            bot.say(self.strings['game_started'])
+            self.games[trigger.sender] = tttgame(trigger)
+            self.join(bot, trigger)
+
+
+    def endgame(self, bot , player_win, place, forced = False):
+        if forced:
+            bot.say(self.strings["adstop"])
+        else:
+            bot.say("[" + TRIS + "] : partita finita in " + place + ". Vincitore: " + player_win , log_chan)
+        del self.games[place]
+
+    def play(self , bot , trigger):
+        if trigger.sender in self.games:
+            self.games[trigger.sender].play(bot , trigger)
+        else:
+            bot.say(self.strings["not_started"])
+
+    def join(self, bot, trigger):
+        if trigger.sender in self.games:
+            self.games[trigger.sender].join(bot, trigger)
+        else:
+            bot.say(self.strings['not_started'])
+
+    def print_grid(self , bot , trigger):
+        if trigger.sender in self.games:
+            self.games[trigger.sender].print_grid(bot)
+        else:
+            bot.say(self.strings["not_started"])
+
+
+
+ttt = tttbot()
 
 @plugin.commands("grid")
-def grid(bot):
-    tttgame.print_grid(bot)
+def grid(bot , trigger):
+    if trigger.sender in game_chan:
+        ttt.print_grid(bot , trigger)
+
+@plugin.commands("ttt")
+def ttt_start(bot , trigger):
+    if trigger.sender in game_chan:
+        bot.say("[" + TRIS + "] : partita INIZIATA in " + trigger.sender + " da: " + trigger.nick , log_chan)
+        ttt.start(bot , trigger)
+
+@plugin.commands("join")
+def ttt_join(bot , trigger):
+    if trigger.sender in game_chan:
+        ttt.join(bot , trigger)
+
+@plugin.commands("adstop")
+def adstop(bot , trigger):
+    if trigger.sender in game_chan:
+        bot.say("[" + TRIS + "] : partita BLOCCATA in " + trigger.sender + " da: " + trigger.nick , log_chan)
+        ttt.endgame(bot , forced = True ,place = trigger.sender , player_win = None )
+
+@plugin.commands("play")
+def play(bot , trigger):
+    if trigger.sender in game_chan:
+        ttt.play(bot , trigger)
