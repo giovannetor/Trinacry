45e2ddeeb6e4782dee0003fff5d1ba55469c225c
GioCoci <74571098+giovannetor@users.noreply.github.com>
2021-08-18 16:30:26 +0200

    removed woggle license

--- briscola/BrisBot2.py
+++ briscola/BrisBot2.py
@@ -1,20 +1,4 @@
-"""
-Woggle - bot framework
 
-  Copyright (C) 2021 Giovanni Coci <giovanni.coci1@yahoo.com>
-
-This file is part of Woggle. Woggle is free software: you can redistribute
-it and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation, version 3.
-
-Woggle is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
 # Version 4.1
 
 import os
