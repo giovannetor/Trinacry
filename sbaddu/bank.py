d68a5d486459f36060081584f4f0df24f04c4584
GioCoci <giovanni.coci1@yahoo.com>
2021-08-24 13:29:54 +0200

    added pay and give, and formats

--- sbaddu/bank.py
+++ sbaddu/bank.py
@@ -1,4 +1,5 @@
-from sopel.plugin import  commands, require_privmsg , example
+from sopel.plugin import  commands,  example , require_admin
+from sopel.formatting import CONTROL_COLOR , colors , CONTROL_BOLD , CONTROL_NORMAL, CONTROL_ITALIC
 
 
 def money_getter(bot , nick):
@@ -9,16 +10,29 @@ def money_getter(bot , nick):
     else:
         return money
 
+def denari():
+    denari_format = CONTROL_BOLD + CONTROL_COLOR +colors.ORANGE + "Denari (₪)" + CONTROL_NORMAL
+    return denari_format
+
+def format_add(money):
+    add = CONTROL_BOLD + "[" + CONTROL_COLOR + colors.LIGHT_GREEN + "+" + CONTROL_NORMAL + CONTROL_BOLD + "]" + CONTROL_NORMAL + \
+          CONTROL_ITALIC + " ADDED " + str(money) + denari() + " to your Bank."
+    return add
+
+def format_rem(money):
+    rem = CONTROL_BOLD + "[" + CONTROL_COLOR + colors.LIGHT_RED + "-" + CONTROL_NORMAL + CONTROL_BOLD + "]" + CONTROL_NORMAL + \
+          CONTROL_ITALIC + " REMOVED " + str(money) + denari() + " from your Bank."
+    return rem
 
 @commands("bank" , "ba")
 @example(".bank" , ".bank gio")
 def bank(bot , trigger):
     if not trigger.group(3):
         money = money_getter(bot , trigger.nick)
-        bot.reply(" you have " + str(money) + "denari in your Bank.")
+        bot.reply(" you have " + str(money) + denari() + " in your Bank.")
     else:
         if trigger.group(3) == "Trinacry" or trigger.group(3) == "trinacry":
-            bot.reply("Darling, I'm the Bank. I own EVERYTHING!!!")
+            bot.reply("Darling, I'm the Bank. I own all of your " + denari() + " MUAHAHA!!")
             return
 
         gender = bot.db.get_nick_value(trigger.group(3) , "pronouns")
@@ -29,23 +43,47 @@ def bank(bot , trigger):
             gender_def = gender.split("/")[2]
 
         money = money_getter(bot , trigger.group(3))
-        bot.reply(trigger.group(3) + "  has " + str(money) + "denari in " + gender_def + " Bank.")
+        bot.reply(trigger.group(3) + "  has " + str(money) + denari() + " in " + gender_def + " Bank.")
 
 @commands("pay")
 @example(".pay gio 50")
 def pay(bot , trigger):
+    if not trigger.group(3) or not trigger.group(4):
+        bot.reply(CONTROL_BOLD + CONTROL_COLOR + colors.RED + "FORMAT:" + CONTROL_NORMAL + " .pay <nick> <denari>")
+        return
+
     money_1 = money_getter(bot , trigger.nick)
     money_2 = money_getter(bot , trigger.group(3))
-
-    to_give = int(trigger.group(4))
+    try:
+        to_give = int(trigger.group(4))
+    except:
+        bot.reply(CONTROL_BOLD + CONTROL_COLOR + colors.RED + "FORMAT:" + CONTROL_NORMAL + " .pay <nick> <denari>")
+        return
 
     if to_give > money_1:
-        bot.reply(" you don't have enough money to pay.")
+        bot.reply(" you don't have enough " + denari() + " to pay.")
         return
 
     money_1 -= to_give
     money_2 += to_give
 
-    bot.db.set_nick_value(trigger.nick , "money" , money_1)
-    bot.db.set_nick_value(trigger.group(3) , "money" , money_2)
+    bot.db.set_nick_value(trigger.nick , "denari" , money_1)
+    bot.db.set_nick_value(trigger.group(3) , "denari" , money_2)
+    bot.say("Succesfully paid " + str(money_2) + denari() + " to " + trigger.group(3))
 
+    bot.say(format_rem(to_give) , trigger.nick)
+    bot.say(format_add(to_give) , trigger.group(3))
+
+@commands("give")
+@require_admin("I'm sorry, but only admins can give money.")
+@example(".give gio 50")
+def give(bot , trigger):
+
+    money = money_getter(bot , trigger.group(3))
+    money += int(trigger.group(4))
+
+    bot.db.set_nick_value(trigger.group(3) , "denari" , money)
+
+    bot.reply(" correctly added " + str(money) + denari() + " to "  + trigger.group(3) + "'s Bank.")
+
+    bot.say(format_add(trigger.group(4)) ,trigger.group(3))
